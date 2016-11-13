7425499e77b516ca8808ec3eae07fa2384c23a78
koinhug@126.com <koinhug@126.com>
2016-11-13 22:40:07 +0800

    双向链表

---
+++ 链表/双向链表/list_node.c
@@ -0,0 +1,16 @@
+#include "double_linklist.h"
+
+/*
+ * Allocates a new list_node_t. NULL on failure.
+ */
+
+list_node_t *
+list_node_new(void *val) {
+    list_node_t *self;
+    if (!(self = LIST_MALLOC(sizeof(list_node_t))))
+        return NULL;
+    self->prev = NULL;
+    self->next = NULL;
+    self->val = val;
+    return self;
+}
